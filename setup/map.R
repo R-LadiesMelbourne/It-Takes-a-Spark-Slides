2b7c3371cac79628307b98fff292771bf13476a5
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-10-22 09:21:35 +0000

    Built site for gh-pages

--- setup/map.R
+++
@@ -1,2 +0,0 @@
-map <- load(url("https://raw.githubusercontent.com/shaziaruybal/2022-05-04-It-takes-a-spark/master/data/Map.rda"))
-map_data <- read_csv(here::here("setup/map_data.csv")) %>% janitor::clean_names()
